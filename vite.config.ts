a308c3119ae116e1b0f39cef03863c407a019b9e
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2025-06-24 10:23:23 -0500

    fixing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/FlightTracker/",
-});
+  base: "/repo-name/", // remove if using custom domain
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
